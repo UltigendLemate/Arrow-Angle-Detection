ecee1b986142d5ad64345c3de9678c479e038a16
Chaitanya <chaitanya.anand@gmail.com>
2022-01-14 00:56:19 +0530

    Learnt to capture vid feeds

---
+++ cvtut2.py
@@ -0,0 +1,16 @@
+import cv2 
+import numpy as np
+
+vid = cv2.VideoCapture(0)
+while True:
+    ret,frame = vid.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',frame)
+    cv2.imshow('frame2',gray)
+
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
